412719ef400d283dbf5000755467fd60501f1e35
krisko <krisenkaasus@gmail.com>
2022-07-19 01:58:40 +0300

    add

---
+++ public/js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#button-A").click(function(){
+      $(".A").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#button-B").click(function(){
+      $(".B").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#button-C").click(function(){
+      $(".C").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#button-D").click(function(){
+      $(".D").toggle();
+    });
+});
+
+
